7a254bd8e3a91c9a23815cea2892aeafc1310e9c
steenzout <pedro.salgado@mediaforge.com>
2015-01-21 14:05:45 -0700

    added section comments. improved syntax test. improved playbook run test.

--- tests/run.sh
+++ tests/run.sh
@@ -4,13 +4,32 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'
 
-ansible-playbook --syntax-check -i inventory/local test.yml
-ansible-playbook -i inventory/local test.yml
 
+# syntax test
+OUTPUT=`ansible-playbook --syntax-check -i inventory/local test.yml 2>&1`
 RETURN_CODE=$?
 if [[ $RETURN_CODE != 0 ]]; then
+  echo -ne "Syntax test: ${RED}FAIL${NC}\n"
+  echo $OUTPUT
   exit $RETURN_CODE
+else
+  echo -ne "Syntax test: ${GREEN}PASS${NC}\n"
 fi
+
+
+# playbook run test
+
+ansible-playbook -i inventory/local test.yml
+RETURN_CODE=$?
+if [[ $RETURN_CODE != 0 ]]; then
+  echo -ne "Playbook run test: ${RED}PASS${NC}\n"
+  exit $RETURN_CODE
+else
+  echo -ne "Playbook run test: ${GREEN}PASS${NC}\n"
+fi
+
+
+# playbook idempotency test
 
 ansible-playbook -i inventory/local test.yml | \
     grep -q 'changed=0.*failed=0' && \
