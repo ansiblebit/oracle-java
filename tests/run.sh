e6f8badae2de01eb83c66cd99f711c1c2cf0382e
steenzout <pedro.salgado@mediaforge.com>
2015-01-21 13:56:02 -0700

    test should fail if running playbook doesn't return a exit code = 0.

--- tests/run.sh
+++ tests/run.sh
@@ -6,6 +6,9 @@ NC='\033[0m'
 
 ansible-playbook --syntax-check -i inventory/local test.yml
 ansible-playbook -i inventory/local test.yml
+
+RETURN_CODE=$?; if [[ $RETURN_CODE != 0 ]]; then exit $RETURN_CODE; fi
+
 ansible-playbook -i inventory/local test.yml | \
     grep -q 'changed=0.*failed=0' && \
     (echo -ne "Idempotence test: ${GREEN}PASS${NC}\n" && exit 0) || \
