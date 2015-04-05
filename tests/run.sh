f784a106d246a7201b8fe24598adbd804e990c59
steenzout <steenzout@ymail.com>
2015-04-04 21:02:32 -0600

    added individual playbook tests.

--- tests/run.sh
+++ tests/run.sh
@@ -4,35 +4,36 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'
 
+DIR="$(dirname "$0")"
 
-# syntax test
-OUTPUT=`ansible-playbook --syntax-check -i inventory/local test.yml 2>&1`
-RETURN_CODE=$?
-if [[ $RETURN_CODE != 0 ]]; then
-  echo -ne "Syntax test: ${RED}FAIL${NC}\n"
-  echo $OUTPUT
-  exit $RETURN_CODE
-else
-  echo -ne "Syntax test: ${GREEN}PASS${NC}\n"
-fi
+cd $DIR
+
+cat requirements.txt | xargs -I {} ansible-galaxy install {}
 
 
-# playbook run test
+# WARNING: box names must match Vagrantfile!
+for box in precise64 trusty64
+do
+    vagrant up $box
 
-ansible-playbook -i inventory/local test.yml
-RETURN_CODE=$?
-if [[ $RETURN_CODE != 0 ]]; then
-  echo -ne "Playbook run test: ${RED}PASS${NC}\n"
-  exit $RETURN_CODE
-else
-  echo -ne "Playbook run test: ${GREEN}PASS${NC}\n"
-fi
+    for test in test_java7.yml test_java8.yml
+    do
 
+        ansible-playbook -i inventory/vagrant -l $box.vagrant.dev $test
+        RETURN_CODE=$?
+        if [[ $RETURN_CODE != 0 ]]; then
+          echo -ne "[$box] playbook run: ${RED}FAILED${NC}\n"
+          exit $RETURN_CODE
+        else
+          echo -ne "[$box] playbook run: ${GREEN}PASS${NC}\n"
+        fi
 
-# playbook idempotency test
+        ansible-playbook -i inventory/vagrant -l $box.vagrant.dev $test | \
+            grep -q 'changed=0.*failed=0' && \
+            echo -ne "[$box] idempotence test: ${GREEN}PASS${NC}\n" || \
+            (echo -ne "[$box] idempotence test: ${RED}FAILED${NC}\n" && exit 1)
 
-ansible-playbook -i inventory/local test.yml | \
-    grep -q 'changed=0.*failed=0' && \
-    (echo -ne "Idempotence test: ${GREEN}PASS${NC}\n" && exit 0) || \
-    (echo -ne "Idempotence test: ${RED}FAIL${NC}\n" && exit 1)
+    done
 
+    vagrant destroy -f $box
+done
