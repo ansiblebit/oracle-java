6ac47169dc7040587d4b3e48e604cc43ba43d005
steenzout <steenzout@ymail.com>
2015-04-27 19:02:03 -0600

    added note.

--- tests/install_role_dependencies.sh
+++ tests/install_role_dependencies.sh
@@ -2,4 +2,5 @@
 
 echo '[INFO] installing Ansible galaxy dependencies...'
 # NOTE: the contents of this file must match the values of the dependencies key in the meta/main.yml file
-ansible-galaxy install -r ${REQUIREMENTS_FILE}
+ansible-galaxy install --ignore-errors -r ${REQUIREMENTS_FILE}
+# NOTE : remove --ignore-errors when your requirements.yml file is not empty
