1e43e7cab35e5a35a481514a00664e8dc37f888d
steenzout <steenzout@ymail.com>
2015-05-04 13:52:40 -0600

    refactoring of testing.

--- tests/test_idempotence.sh
+++ tests/test_idempotence.sh
@@ -3,20 +3,28 @@
 #
 # Bash script to run idempotence tests.
 #
-# version: 1.0
+# version: 1.2
 #
-# mandatory variables:
+# usage:
 #
-#   from environment.sh
-#   - INVENTORY : the inventory to be used to run the test against.
-#   - PLAYBOOK : the path to the Ansible playbook.
+#   test_idempotence [options]
 #
-#   from <playbook_name>.sh
-#   - BOX : the name of the vagrant box
+# options:
 #
-# optional variables:
+#   --box       The name of the Vagrant box or host name
+#   --inventory The Ansible inventory in the form of a file or string "host,"
+#   --playbook  The path to the Ansible test playbook
 #
-#   - VIRTUALENV_NAME : the name of the virtualenv being used.
+# example:
+#
+#   # on localhost
+#   bash test_idempotence.sh
+#
+#   # on a Vagrant box
+#   bash test_idempotence.sh \
+#       --box precise64
+#       --inventory .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory
+#
 #
 # author(s):
 #   - Pedro Salgado <steenzout@ymail.com>
@@ -30,11 +38,58 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 # SGR code to set text color (foreground) to no color.
 NC='\033[0m'
+# The idempotence pass criteria.
+PASS_CRITERIA="changed=0.*failed=0"
+
+# the name of the virtualenv
+VIRTUALENV_NAME=$(which python | awk -F / 'NF && NF-2 { print ( $(NF-2) ) }')
+
+
+while [[ $# > 1 ]]
+do
+key="$1"
+
+    case $key in
+
+        --box)
+        # the name of the Vagrant box or host name
+        BOX="$2"
+        shift;;
+
+        --inventory)
+        # the Ansible inventory in the form of a file or string "host,"
+        INVENTORY="$2"
+        shift;;
+
+        --playbook)
+        # the path to the Ansible test playbook
+        PLAYBOOK="$2"
+        shift;;
+
+        *)
+        # unknown option
+        ;;
+
+    esac
+    shift
+done
+
+# the name of the Vagrant box or host name
+BOX=${1:-localhost}
+# the Ansible inventory in the form of a file or string "host,"
+INVENTORY=${2:-'localhost,'}
+# the path to the Ansible test playbook
+PLAYBOOK=${3:-test.yml}
 # the logfile to hold the output of the playbook run
 LOGFILE="log/${BOX}_${VIRTUALENV_NAME}.log"
 
+EXTRA_ARGS=''
+if [ $BOX == "localhost" ]; then
+    EXTRA_ARGS="--skip-tags=vagrant --connection=local"
+fi
+
 echo "[INFO] ${BOX} ${VIRTUALENV_NAME} running idempotence test..."
-ansible-playbook -i ${INVENTORY} --limit ${BOX} ${PLAYBOOK} 2>&1 | tee ${LOGFILE} | \
+ansible-playbook -i ${INVENTORY} --limit ${BOX} ${PLAYBOOK} ${EXTRA_ARGS} 2>&1 | tee ${LOGFILE} | \
     grep "${BOX}" | grep -q "${PASS_CRITERIA}" && \
     echo -ne "[TEST] ${BOX} ${VIRTUALENV_NAME} idempotence : ${GREEN}PASS${NC}\n" || \
     (echo -ne "[TEST] ${BOX} ${VIRTUALENV_NAME} idempotence : ${RED}FAILED${NC} ${PASS_CRITERIA}\n" && exit 1)
