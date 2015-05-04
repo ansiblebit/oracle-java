8c004d668f03ffc39b9a9930b8f8f795a93a1c6d
steenzout <steenzout@ymail.com>
2015-05-04 16:04:16 -0600

    implemented idempotence test through callback module.

---
+++ tests/plugins/callback/idempotence.py
@@ -0,0 +1,23 @@
+import sys
+import os
+
+
+class CallbackModule(object):
+    """
+    This callback module tells you how long your plays ran for.
+    """
+
+    def __init__(self):
+        pass
+
+    def playbook_on_stats(self, stats):
+
+        if u'idempotence' in self.playbook.extra_vars.keys():
+
+            if (len(stats.changed) > 0):
+                print 'idempotence test failed! changed=%s > 0'
+                sys.exit(os.EX_SOFTWARE)
+
+            if (len(stats.failures) > 0):
+                print 'idempotence test failed! failures=%s > 0'
+                sys.exit(os.EX_SOFTWARE)
