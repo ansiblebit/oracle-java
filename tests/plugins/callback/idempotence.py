f8b130e514ab9072a8dd805c9cee85c832b66160
Pedro Salgado <steenzout@ymail.com>
2016-03-21 14:00:01 -0600

    improve test scripts

--- tests/plugins/callback/idempotence.py
+++ tests/plugins/callback/idempotence.py
@@ -1,29 +1,49 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import (absolute_import, print_function)
+
 import sys
 import os
 
-VAR_IDEMPOTENCE = u'idempotence'
+from ansible import constants as C
+from ansible.constants import mk_boolean
+
+try:
+    from ansible.plugins.callback import CallbackBase
+    parent = CallbackBase
+except ImportError:
+    parent = object
 
 
-class CallbackModule(object):
+VAR_IDEMPOTENCE = u'IDEMPOTENCE'
+
+
+class CallbackModule(parent):
     """
-    This callback module performs the idempotence test whenever the 'idempotence' variable is set to True.
+    This callback module performs the idempotency test whenever the 'idempotency' variable is set to True.
     """
+    CALLBACK_VERSION = 2.0
+    CALLBACK_NAME = 'idempotency'
 
     def __init__(self):
-        pass
+        self.playbook = None
+        self.enabled = mk_boolean(os.getenv(VAR_IDEMPOTENCE, 'no'))
+
+        super(CallbackModule, self).__init__()
 
     def playbook_on_stats(self, stats):
 
-        if (u'%s' % VAR_IDEMPOTENCE) in self.playbook.extra_vars.keys() and self.playbook.extra_vars[VAR_IDEMPOTENCE]:
-
+        if self.enabled:
             if len(stats.dark) > 0:
-                print ('idempotence test failed! unreachable=%s > 0') % stats.dark
+                self._display.warning('idempotency test failed: unreachable=%s > 0' % stats.dark)
                 sys.exit(os.EX_SOFTWARE)
-
             if len(stats.changed) > 0:
-                print ('idempotence test failed! changed=%s > 0') % stats.changed
+                self._display.warning('idempotency test failed: changed=%s > 0' % stats.changed)
+                sys.exit(os.EX_SOFTWARE)
+            if len(stats.failures) > 0:
+                self._display.warning('idempotency test failed: failures=%s > 0' % stats.failures)
                 sys.exit(os.EX_SOFTWARE)
 
-            if len(stats.failures) > 0:
-                print ('idempotence test failed! failures=%s > 0') % stats.failures
-                sys.exit(os.EX_SOFTWARE)
+    def v2_playbook_on_stats(self, stats):
+        """Verify that playbook ran without any changes or failures."""
+        self.playbook_on_stats(stats)
